96f5b1c8aee44b18f586b621ac8a2c7780e170aa
KenDual <maiphuhai123@gmail.com>
2025-10-20 13:32:30 +0700

    add UserDashboardController and view for UserDashboard

---
+++ OnlineJewelryStore/Controllers/UserDashboardController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJewelryStore.Controllers
+{
+    using System.Web.Mvc;
+    namespace OnlineJewelryStore.Controllers
+    {
+        public class UserDashboardController : Controller
+        {
+            [HttpGet]
+            public ActionResult Index()
+            {
+                ViewBag.Active = "Dashboard"; return View();
+            }
+            [HttpGet]
+            public ActionResult Orders()
+            {
+                ViewBag.Active = "Orders"; return View();
+            }
+            [HttpGet]
+            public ActionResult PaymentMethod()
+            { 
+                ViewBag.Active = "payment"; return View(); 
+            }
+            [HttpGet] 
+            public ActionResult Address() 
+            { 
+                ViewBag.Active = "address"; return View(); 
+            }
+            [HttpGet] 
+            public ActionResult AccountDetails() 
+            { 
+                ViewBag.Active = "account"; return View(); 
+            }
+        }
+    }
+}
