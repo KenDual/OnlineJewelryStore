abf186f4ac2b91d3a9a523de93586c3c339c3593
KenDual <maiphuhai123@gmail.com>
2025-10-20 15:56:55 +0700

    LamChiBao

---
+++ OnlineJewelryStore/Controllers/FeatureController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJewelryStore.Controllers
+{
+    public class FeatureController : Controller
+    {
+        // GET: Feature
+        public ActionResult Home()
+        {
+            ViewBag.Active = "Home"; return View();
+        }
+        [HttpGet]
+        public ActionResult Shop()
+        {
+            ViewBag.Active = "Shop"; return View();
+        }
+        [HttpGet]
+        public ActionResult Wishlist()
+        {
+            ViewBag.Active = "Wishlist"; return View();
+        }
+        [HttpGet]
+        public ActionResult Blog()
+        {
+            ViewBag.Active = "Blog"; return View();
+        }
+        [HttpGet]
+        public ActionResult Contact_us()
+        {
+            ViewBag.Active = "Contact_us"; return View();
+        }
+        public ActionResult Account()
+        {
+            ViewBag.Active = "Account"; return View();
+        }
+    }
+}
